0e2ee10e96c5c24f726183d2bb1df0333f6b653c
abeljemal22 <abel.j3938@gmail.com>
2022-11-14 07:37:25 -0800

    hsh

---
+++ hsh.c
@@ -0,0 +1,8 @@
+#include "shell.h"
+
+/**
+ * hsh - main shell loop
+ * @info: the parameter & return info struct
+ * @av: the argument vector from main()
+ * Return: 0 on success, 1 on error, or error code
+ */
