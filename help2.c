cb53fbc84e5278a3b0ceb96752d8a45202d0b164
Abelj <111173212+abeljemal22@users.noreply.github.com>
2022-11-17 04:13:35 -0400

    shell

--- help2.c
+++ help2.c
@@ -1,40 +1,139 @@
 #include "main.h"
 
 /**
- * aux_help - Help information for the builtin help.
- * Return: no return
+ *_strtok_r - tokenizes a string
+ *@string: string to be tokenized
+ *@delim: delimiter to be used to tokenize the string
+ *@save_ptr: pointer to be used to keep track of the next token
+ *
+ *Return: The next available token
  */
-void aux_help(void)
+char *_strtok_r(char *string, char *delim, char **save_ptr)
 {
-	char *help = "help: help [-dms] [pattern ...]\n";
+	char *finish;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tDisplay information about builtin commands.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Displays brief summaries of builtin commands.\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	if (string == NULL)
+		string = *save_ptr;
+
+	if (*string == '\0')
+	{
+		*save_ptr = string;
+		return (NULL);
+	}
+
+	string += _strspn(string, delim);
+	if (*string == '\0')
+	{
+		*save_ptr = string;
+		return (NULL);
+	}
+
+	finish = string + _strcspn(string, delim);
+	if (*finish == '\0')
+	{
+		*save_ptr = finish;
+		return (string);
+	}
+
+	*finish = '\0';
+	*save_ptr = finish + 1;
+	return (string);
 }
+
 /**
- * aux_help_alias - Help information for the builtin alias.
- * Return: no return
+ * _atoi - changes a string to an integer
+ * @s: the string to be changed
+ *
+ * Return: the converted int
  */
-void aux_help_alias(void)
+int _atoi(char *s)
 {
-	char *help = "alias: alias [-p] [name[=value]...]\n";
+	unsigned int n = 0;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tDefine or display aliases.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
+	do {
+		if (*s == '-')
+			return (-1);
+		else if ((*s < '0' || *s > '9') && *s != '\0')
+			return (-1);
+		else if (*s >= '0'  && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	return (n);
 }
+
 /**
- * aux_help_cd - Help information for the builtin alias.
- * Return: no return
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated with a call to malloc
+ * @old_size: size of ptr
+ * @new_size: size of the new memory to be allocated
+ *
+ * Return: pointer to the address of the new memory block
  */
-void aux_help_cd(void)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
+	void *temp_block;
+	unsigned int i;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tChange the shell working directory.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
+	if (ptr == NULL)
+	{
+		temp_block = malloc(new_size);
+		return (temp_block);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		temp_block = malloc(new_size);
+		if (temp_block != NULL)
+		{
+			for (i = 0; i < min(old_size, new_size); i++)
+				*((char *)temp_block + i) = *((char *)ptr + i);
+			free(ptr);
+			return (temp_block);
+		}
+		else
+			return (NULL);
+
+	}
 }
+
+/**
+ * ctrl_c_handler - handles the signal raised by CTRL-C
+ * @signum: signal number
+ *
+ * Return: void
+ */
+void ctrl_c_handler(int signum)
+{
+	if (signum == SIGINT)
+		print("\n($) ", STDIN_FILENO);
+}
+
+/**
+ * remove_comment - removes/ignores everything after a '#' char
+ * @input: input to be used
+ *
+ * Return: void
+ */
+void remove_comment(char *input)
+{
+	int i = 0;
+
+	if (input[i] == '#')
+		input[i] = '\0';
+	while (input[i] != '\0')
+	{
+		if (input[i] == '#' && input[i - 1] == ' ')
+			break;
+		i++;
+	}
+	input[i] = '\0';
+}
+
