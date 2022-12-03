79c4c6c16f158f3e9e3bc15a6a116763add995c6
Abelj <111173212+abeljemal22@users.noreply.github.com>
2022-12-03 08:14:17 +0300

    help

--- help.c
+++ help.c
@@ -1,80 +1,103 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * aux_help_env - Help information for the builtin env
- * Return: no return
+ * tokenizer - tokenizes input and stores it into an array
+ *@input_string: input to be parsed
+ *@delim: delimiter to be used, needs to be one character string
+ *
+ *Return: array of tokens
  */
-void aux_help_env(void)
+
+char **tokenizer(char *input_string, char *delim)
 {
-	char *help = "env: env [option] [name=value] [command [args]]\n\t";
+	int num_delim = 0;
+	char **av = NULL;
+	char *token = NULL;
+	char *save_ptr = NULL;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Print the enviroment of the shell.\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	token = _strtok_r(input_string, delim, &save_ptr);
 
-}
-/**
- * aux_help_setenv - Help information for the builtin setenv
- * Return: no return
- */
-void aux_help_setenv(void)
-{
+	while (token != NULL)
+	{
+		av = _realloc(av, sizeof(*av) * num_delim, sizeof(*av) * (num_delim + 1));
+		av[num_delim] = token;
+		token = _strtok_r(NULL, delim, &save_ptr);
+		num_delim++;
+	}
 
-	char *help = "setenv: setenv (const char *name, const char *value,";
+	av = _realloc(av, sizeof(*av) * num_delim, sizeof(*av) * (num_delim + 1));
+	av[num_delim] = NULL;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "int replace)\n\t";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Add a new definition to the environment\n";
-	write(STDOUT_FILENO, help, _strlen(help));
-}
-/**
- * aux_help_unsetenv - Help information for the builtin unsetenv
- * Return: no return
- */
-void aux_help_unsetenv(void)
-{
-	char *help = "unsetenv: unsetenv (const char *name)\n\t";
-
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Remove an entry completely from the environment\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	return (av);
 }
 
-
 /**
- * aux_help_general - Entry point for help information for the help builtin
- * Return: no return
+ *print - prints a string to stdout
+ *@string: string to be printed
+ *@stream: stream to print out to
+ *
+ *Return: void, return nothing
  */
-void aux_help_general(void)
+void print(char *string, int stream)
 {
-	char *help = "^-^ bash, version 1.0(1)-release\n";
+	int i = 0;
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "These commands are defined internally.Type 'help' to see the list";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Type 'help name' to find out more about the function 'name'.\n\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "[dir]\nexit: exit [n]\n  env: env [option] [name=value] [command ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "[args]]\n  setenv: setenv [variable] [value]\n  unsetenv: ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "unsetenv [variable]\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+	for (; string[i] != '\0'; i++)
+		write(stream, &string[i], 1);
 }
-/**
- * aux_help_exit - Help information fot the builint exit
- * Return: no return
- */
-void aux_help_exit(void)
-{
-	char *help = "exit: exit [n]\n Exit shell.\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Exits the shell with a status of N. If N is ommited, the exit";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "statusis that of the last command executed\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+/**
+ *remove_newline - removes new line from a string
+ *@str: string to be used
+ *
+ *
+ *Return: void
+ */
+
+void remove_newline(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			break;
+		i++;
+	}
+	str[i] = '\0';
+}
+
+/**
+ *_strcpy - copies a string to another buffer
+ *@source: source to copy from
+ *@dest: destination to copy to
+ *
+ * Return: void
+ */
+
+void _strcpy(char *source, char *dest)
+{
+	int i = 0;
+
+	for (; source[i] != '\0'; i++)
+		dest[i] = source[i];
+	dest[i] = '\0';
+}
+
+/**
+ *_strlen - counts string length
+ *@string: string to be counted
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *string)
+{
+	int len = 0;
+
+	if (string == NULL)
+		return (len);
+	for (; string[len] != '\0'; len++)
+		;
+	return (len);
 }
