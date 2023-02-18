db738a7ef8937576484e83853ec474d5152923b0
Abelj <111173212+abeljemal22@users.noreply.github.com>
2023-02-18 17:26:09 +0300

    Update bultin.c

--- bultin.c
+++ bultin.c
@@ -1,62 +1,52 @@
 #include "shell.h"
 
-/**
- *env - prints the current_environnement
- *@tokenized_command: command entered
- *
- *Return: void
- */
+void check_builtin(char **args);
+void sh_exit(char **args);
+void sh_env(char **args);
 
-void env(char **tokenized_command __attribute__((unused)))
+/**
+ *check_builtin - checks if command is a builtin command
+ *@args: pointer array
+ */
+void check_builtin(char **args)
 {
 	int i;
 
-	for (i = 0; environ[i] != NULL; i++)
+	builtin_t builtins[] = {
+		{"exit", sh_exit},
+		{"env", sh_env},
+		{NULL, NULL},
+	};
+
+	for (i = 0; builtins[i].name != NULL; i++)
 	{
-		print(environ[i], STDOUT_FILENO);
-		print("\n", STDOUT_FILENO);
+		if (_strcmp(args[0], builtins[i].name) == 0)
+		{
+			builtins[i].func(args);
+			return;
+		}
 	}
 }
 
 /**
- * quit - exits the shell
- * @tokenized_command: command entered
- *
- * Return: void
+ *sh_exit - exits the shell
+ *@args: argument array
  */
-
-void quit(char **tokenized_command)
+void sh_exit(char **args)
 {
-	int num_token = 0, arg;
+	(void)args;
+	exit(0);
+}
 
-	for (; tokenized_command[num_token] != NULL; num_token++)
-		;
-	if (num_token == 1)
-	{
-		free(tokenized_command);
-		free(line);
-		free(commands);
-		exit(status);
-	}
-	else if (num_token == 2)
-	{
-		arg = _atoi(tokenized_command[1]);
-		if (arg == -1)
-		{
-			print(shell_name, STDERR_FILENO);
-			print(": 1: exit: Illegal number: ", STDERR_FILENO);
-			print(tokenized_command[1], STDERR_FILENO);
-			print("\n", STDERR_FILENO);
-			status = 2;
-		}
-		else
-		{
-			free(line);
-			free(tokenized_command);
-			free(commands);
-			exit(arg);
-		}
-	}
-	else
-		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+/**
+ *sh_env - lists the environment variables
+ *@args: argument array
+ */
+void sh_env(char **args)
+{
+	char **env = environ;
+
+	while (*env)
+		printf("%s\n", *env++);
+	(void)args;
 }
